cdeba506e33bf7da6dd0b886e64dff9dc668ee29
TagRomEdu <qwertystyles@gmail.com>
2024-02-06 10:53:31 +0500

    added context

--- main_app/views.py
+++ main_app/views.py
@@ -2,8 +2,17 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'main_app/index.html')
+    context = {
+        'title': 'Home - Главная',
+        'content': 'Магазин мебели'
+    }
+    return render(request, 'main_app/index.html', context)
 
 
 def about(request):
-    return render(request, 'main_app/about.html')
+    context = {
+        'title': 'Home - О нас',
+        'content': 'Мы ооочень крутые!',
+        'pohvala': 'Наша мебель служит веками (гарантия не предоставляется).'
+    }
+    return render(request, 'main_app/about.html', context)
