4e944a8d86e53063da0e6c2628c2288b498991ac
TagRomEdu <qwertystyles@gmail.com>
2024-02-09 11:10:15 +0500

    deleted category from main_app and created category in catalog_app

--- catalog_app/models.py
+++ catalog_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, **NULLABLE)
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
